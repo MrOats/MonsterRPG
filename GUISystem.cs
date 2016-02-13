066b4177ea8d3140281f1c202afedb42ba8db716
MrOats <snarkatt@gmail.com>
2016-02-13 13:43:48 -0600

    Fixed fonts again

--- GUISystem.cs
+++ GUISystem.cs
@@ -16,15 +16,16 @@ public class GUISystem: Form
 
 		start_program.Location=new Point(0,40);
 		start_program.Text="Start Fighting!";
+		start_program.Font=new Font("Verdana",9F,System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
 		start_program.Size=new Size(80,35);
 		start_program.Click+=new EventHandler(Exec_Monster.beginfight);
 
-		displaybar.Size=new Size(200,30);
+		displaybar.Size=new Size(250,30);
 		displaybar.Font=new Font("Verdana",10F,System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
 
 		wonloss_status.Location=new Point(300,0);
-		wonloss_status.Size=new Size(200,30);
-		wonloss_status.Font=new Font("Verdana",10F,System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
+		wonloss_status.Size=new Size(400,30);
+		wonloss_status.Font=new Font("Verdana",20F,System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
 		Controls.Add(start_program);
 		Controls.Add(displaybar);
 		Controls.Add(wonloss_status);
